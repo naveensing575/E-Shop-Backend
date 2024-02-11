1838bf9178677bad0c8d778e2b6920437b0c990d
naveen.singh <naveen.singh@itelasoft.com>
2024-02-11 13:33:39 +0530

    removed unused routes

--- src/routes/productRoutes.ts
+++ src/routes/productRoutes.ts
@@ -8,8 +8,6 @@ const router = express.Router();
 router.get('/', authenticate, ProductController.getAllProducts);
 router.get('/categories', authenticate, ProductController.getAllCategories);
 router.get('/:id', authenticate,  ProductController.getProductById);
-// router.put('/:productId', ProductController.updateProduct);
-// router.delete('/:productId', ProductController.deleteProduct);
 
 
 export default router;
